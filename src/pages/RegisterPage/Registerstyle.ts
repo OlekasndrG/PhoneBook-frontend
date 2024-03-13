5865e1e65106484bb808d41f152e491edfc9eb32
Olekasndr Gubskiy <infernokgg@gmail.com>
2024-03-13 21:08:49 +0200

    Update registerstyle.ts
    minor

--- src/pages/RegisterPage/Registerstyle.ts
+++ src/pages/RegisterPage/Registerstyle.ts
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-export const H1 = styled.h1`
-  background-color: red;
-`;
+export const H1 = styled.h1``;
 export const RegisterPageContainer = styled.div`
   display: flex;
   flex-direction: column;
